6a935f741d73b7580f1e51d91a1d874f3a816dc3
Amorebietako Udala <informatika@amorebieta.eus>
2019-06-12 08:01:56 +0200

    Fixed dependencies.

--- config/bundles.php
+++ config/bundles.php
@@ -15,8 +15,9 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
     FOS\UserBundle\FOSUserBundle::class => ['all' => true],
+    FR3D\LdapBundle\FR3DLdapBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     AmorebietakoUdala\SMSServiceBundle\SMSServiceBundle::class => ['all' => true],
     FOS\RestBundle\FOSRestBundle::class => ['all' => true],
     JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
-    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
