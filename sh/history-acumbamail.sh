a11807b28794c3f52c41fc950b8fa75ba9f4c709
Amorebietako Udala <informatika@amorebieta.eus>
2019-12-09 09:57:08 +0100

    - HistoryCommand: Fixed acumbamail command to fetch histories every hour.

--- sh/history-acumbamail.sh
+++ sh/history-acumbamail.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-start_date=$(date '+%Y/%m/%d %H'):00
-end_date=$(date --date='+1 hour' '+%Y/%m/%d %H'):00
+start_date=$(date --date='-1 hour' '+%Y/%m/%d %H'):00
+end_date=$(date '+%Y/%m/%d %H'):59:59
 
 cd ..
 php bin/console app:sms-history-acumbamail "$start_date" "$end_date"
